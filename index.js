2d046150c71f9d1264914fdfd4391022d49ab2f3
Samster91 <cester.samuele@gmail.com>
2020-11-12 16:01:00 +0100

    FIX: changed abis variable

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 const COMP_abi = require('./abis/COMP.js').abi;
 const ERC20_abi = require('./abis/ERC20.js').abi;
-const IdleTokenV4_abi = require('./abis/IdleTokenV4.js').abi;
-const IdleTokenV3_abi = require('./abis/IdleTokenV3.js').abi;
-const uniswapRouter_abi = require('./abis/uniswapRouter.js').abi;
+const IdleTokenV4 = require('./abis/IdleTokenV4.js').abi;
+const IdleTokenV3 = require('./abis/IdleTokenV3.js').abi;
+const uniswapRouter = require('./abis/uniswapRouter.js').abi;
 const UniswapExchangeInterface = require('./abis/UniswapExchangeInterface.js').abi;
 
 async function main(){
@@ -19,92 +19,92 @@ async function main(){
 	// Idle tokens info
 	const contracts = {
 		idleDAIYieldV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'DAI',
 		  address:'0x3fe7940616e5bc47b0775a0dccf6237893353bb4'
 		},
 		idleDAIYieldV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'DAI',
 		  address:'0x78751b12da02728f467a44eac40f5cbc16bd7934'
 		},
 		idleUSDCYieldV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'USDC',
 		  address:'0x5274891bEC421B39D23760c04A6755eCB444797C'
 		},
 		idleUSDCYieldV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'USDC',
 		  address:'0x12B98C621E8754Ae70d0fDbBC73D6208bC3e3cA6'
 		},
 		idleUSDTYieldV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'USDT',
 		  address:'0xF34842d05A1c888Ca02769A633DF37177415C2f8'
 		},
 		idleUSDTYieldV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'USDT',
 		  address:'0x63D27B3DA94A9E871222CB0A32232674B02D2f2D'
 		},
 		idleSUSDYieldV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'SUSD',
 		  address:'0xf52cdcd458bf455aed77751743180ec4a595fd3f'
 		},
 		idleSUSDYieldV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'SUSD',
 		  address:'0xe79e177d2a5c7085027d7c64c8f271c81430fc9b'
 		},
 		idleTUSDYieldV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'TUSD',
 		  address:'0xc278041fDD8249FE4c1Aad1193876857EEa3D68c'
 		},
 		idleTUSDYieldV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'TUSD',
 		  address:'0x51C77689A9c2e8cCBEcD4eC9770a1fA5fA83EeF1'
 		},
 		idleWBTCYieldV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'WBTC',
 		  address:'0x8C81121B15197fA0eEaEE1DC75533419DcfD3151'
 		},
 		idleWBTCYieldV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'WBTC',
 		  address:'0xD6f279B7ccBCD70F8be439d25B9Df93AEb60eC55'
 		},
 		idleDAISafeV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'DAI',
 		  address:'0xa14ea0e11121e6e951e87c66afe460a00bcd6a16'
 		},
 		idleDAISafeV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'DAI',
 		  address:'0x1846bdfDB6A0f5c473dEc610144513bd071999fB'
 		},
 		idleUSDCSafeV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'USDC',
 		  address:'0x3391bc034f2935ef0e1e41619445f998b2680d35'
 		},
 		idleUSDCSafeV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'USDC',
 		  address:'0xcDdB1Bceb7a1979C6caa0229820707429dd3Ec6C'
 		},
 		idleUSDTSafeV4:{
-		  abi:IdleTokenV4_abi,
+		  abi:IdleTokenV4,
 		  underlyingToken:'USDT',
 		  address:'0x28fAc5334C9f7262b3A3Fe707e250E01053e07b5'
 		},
 		idleUSDTSafeV3:{
-		  abi:IdleTokenV3_abi,
+		  abi:IdleTokenV3,
 		  underlyingToken:'USDT',
 		  address:'0x42740698959761baf1b06baa51efbd88cb1d862b'
 		}
@@ -123,14 +123,14 @@ async function main(){
 		TUSD: new web3.eth.Contract(ERC20_abi,'0x0000000000085d4780b73119b644ae5ecd22b376'),
 		WBTC: new web3.eth.Contract(ERC20_abi,'0x2260fac5e5542a773aa44fbcfedf7c193bc2c599')
 	};
-  
+
 	// Initialize COMP
 	const COMPAddr = '0xc00e94cb662c3520282e6f5717214004a7f26888';
 	const compContract = new web3.eth.Contract(COMP_abi, COMPAddr);
 
 	// Initialize Uniswap Router
 	const uniswapRouterAddr = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
-	const uniswapRouter = new web3.eth.Contract(uniswapRouter_abi, uniswapRouterAddr);
+	const UniswapRouter = new web3.eth.Contract(uniswapRouter, uniswapRouterAddr);
 
 	// Initialize Uniswap V1
 	const UNI_V1_WBTC_ETH = '0x4d2f5cFbA55AE412221182D8475bC85799A5644b';
@@ -152,7 +152,7 @@ async function main(){
 		const one = BNify('1000000000000000000');
 		const DAIAddr = '0x6b175474e89094c44da98b954eedeac495271d0f';
 		const path = [DAIAddr,'0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',tokenAddr];
-		const unires = await uniswapRouter.methods.getAmountsIn(one.toFixed(),path).call();
+		const unires = await UniswapRouter.methods.getAmountsIn(one.toFixed(),path).call();
 		if (unires){
 		  return BNify(unires[0]).div(one);
 		}
